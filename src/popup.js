6d28cc029ecf4c49384462297d9fd0bb40d1c6a1
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2024-03-07 10:54:17 +0100

    Improve theme toggle and add popover functionality
    This commit refactors the theme toggle function in 'popup.js' for cleaner code, and removes previously hard-coded SVGs for both dark and light modes. It also introduces a popover functionality for action buttons, providing additional information on hover in 'popup.html'. Updates were made to 'popup.css' to visually support this new feature. This enhancement should improve the user experience and accessibility by providing immediate context for each button action.

--- src/popup.js
+++ src/popup.js
@@ -258,24 +258,6 @@ function copyColor(colorDiv) {
 }
 
 
-/**
- * Returns an SVG string representing a dark mode icon.
- *
- * @returns {string} The SVG string.
- */
-function getSvgDarkMode() {
-  return `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z"/></svg>`;
-}
-
-/**
- * Retrieves the SVG representation of the light mode.
- *
- * @returns {string} The SVG representation of the light mode.
- */
-function getSvgLightMode() {
-  return `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z"/></svg>`;
-}
-
 
 /**
  * Updates the HTML attribute 'data-theme' based on the current theme and toggles the icon on a button element.
@@ -283,11 +265,10 @@ function getSvgLightMode() {
  * @return {undefined}
  */
 function updateHtmlDataThemeAttributeAndToggleIcon() {
-  if (data.currentTheme === 'dark') {
-    btnToggleTheme.innerHTML = getSvgDarkMode();
-  } else {
-    btnToggleTheme.innerHTML = getSvgLightMode();
-  }
+  btnToggleTheme.classList.remove('if');
+  btnToggleTheme.classList.remove('else');
+  btnToggleTheme.classList.add((data.currentTheme === 'dark')?'if': 'else');
+
   document.querySelector('html').setAttribute('data-theme', data.currentTheme);
 }
 
@@ -317,6 +298,27 @@ function renderCSSCustomPropertyMap(){
  * @return {void}
  */
 function initListener() {
+  document
+    .querySelectorAll('[popovertarget]')
+    .forEach(ele => ele
+      .addEventListener('mouseover', ()=> {
+        const popovertarget = ele.getAttribute('popovertarget');
+        console.log('mouseover', popovertarget)
+        if (popovertarget) {
+          document.getElementById(popovertarget).classList.add('popover-open');
+        }
+      }));
+  document
+    .querySelectorAll('[popovertarget]')
+    .forEach(ele => ele
+      .addEventListener('mouseout', ()=> {
+        const popovertarget = ele.getAttribute('popovertarget');
+        if (popovertarget) {
+          document.getElementById(popovertarget).classList.remove('popover-open');
+
+        }
+      }));
+
   btnOk.addEventListener("click", grabColors);
 
   btnToggleDesignMode.addEventListener("click", async () => {
@@ -340,15 +342,16 @@ function initListener() {
     return new Promise((resolve, _reject)=> setTimeout(resolve, ms));
   }
 
+
   btnCaptureScreen.addEventListener("click", async () => {
+    const ret = [];
     // window.close(); // close popup
     // chrome.windows.getCurrent (currentWindow => {
     //   chrome.windows.update(currentWindow.id, {  width: 25, height: 25  });
     // });
     //
     console.log('go go go...')
-    let c = await captureVisibleTab();
-    console.log(c);
+
 
     // window.close();
     // setTimeout(async () => {
@@ -365,6 +368,11 @@ function initListener() {
     let y = 0;
     await scrollToYPos(0);
     while (y < canvasHeight) {
+      let c = await captureVisibleTab();
+      ret.push({
+        imgUrl: c
+      });
+      console.log(c);
       y += viewportHeight;
       console.log('scrolled. Now:  y', y);
       await wait(500);
@@ -378,6 +386,7 @@ function initListener() {
     //     const s = JSON.stringify(res, null, 0);
     //     setLabelText(`Capture: ${s}`);
     //   });
+    return ret;
   });
 
 
